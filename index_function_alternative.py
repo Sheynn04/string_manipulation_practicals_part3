fc383335b997885d30e38fd8550609f56c30fbef
ycz <yczaubhreydy@gmail.com>
2025-04-01 21:34:22 +0800

    pseudocode and user inputs

---
+++ index_function_alternative.py
@@ -0,0 +1,11 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+#1. Ask for the user input.
+user_input = input("Input any string: ")
+
+# 2. Get the letter to find from the user.
+index = input("What letter to find? : ")
+
+# 3. Count the characters before the indexed character.
+
+# 4. Print the length of characters before the indexed character.
