709ae13aaea1407a5c096937aaf8e5564e8bcedc
ycz <yczaubhreydy@gmail.com>
2025-04-01 16:30:22 +0800

    used endswith function and print the result without the suffix

--- removesuffix_alternative.py
+++ removesuffix_alternative.py
@@ -2,8 +2,14 @@
 
 # 1. Ask for the user inout.
 string_input = input("Input any string: ")
+
 # 2. Assign a suffix
 suffix = "dy"
-# 3. Remove the suffix from the user input without using removesuffix().
+
+# 3. Create an if statement and use endswith() function as an alternative for removesuffix().
+if string_input.endswith(suffix):
 
 # 4. Print the result.
+    print(string_input[:-len(suffix)])
+else:
+    print(string_input)
