383d87a6e4809a493d6c29c520f14a7a2e26636c
ycz <yczaubhreydy@gmail.com>
2025-04-01 20:11:43 +0800

    pseudocode

---
+++ isupper_function_alternative.py
@@ -0,0 +1,9 @@
+# Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+
+# 1. Ask for user inpu.
+
+# 2. Assign True to upper case letters.
+
+# 3. Access each character in the input.
+
+# 4. Check if characters are upper. If yes print true, if no print false.
