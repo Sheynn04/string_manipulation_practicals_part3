71bff087c7744c3e4ff06fc0014d908aa3b40481
ycz <yczaubhreydy@gmail.com>
2025-04-01 20:21:26 +0800

    islower alternative (for loop)

--- isupper_function_alternative.py
+++ isupper_function_alternative.py
@@ -3,8 +3,14 @@
 # 1. Ask for user input. 
 string_input = input("Input any string: ")
 
-# 2. Assign True to upper case letters.
+# 2. Assign True to lower case letters.
+is_lower = True
 
 # 3. Access each character in the input.
+for char in string_input:
 
-# 4. Check if characters are upper. If yes print true, if no print false.
+# 4. Check if characters are lower. If yes print true, if no print false.
+    if not ("a" <= char <= "z"):
+        is_lower = False
+        break
+print(is_lower)
