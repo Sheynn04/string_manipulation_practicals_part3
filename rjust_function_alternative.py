ba234d9586dd077e2fcaa9c19f2a015a8648146c
ycz <yczaubhreydy@gmail.com>
2025-04-01 20:45:30 +0800

    user input

--- rjust_function_alternative.py
+++ rjust_function_alternative.py
@@ -1,7 +1,7 @@
 # Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 
 # 1. Ask for user input. 
-
+string_input = input("Enter any string: ")
 # 2. Assign a width for the statement.
 
 # 3. Add spaces to the user input if it's under the width
