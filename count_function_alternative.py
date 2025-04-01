5b1490c73784627134f592dd16da5446f7b40db5
ycz <yczaubhreydy@gmail.com>
2025-04-01 21:13:35 +0800

    pseudocode and user input

--- count_function_alternative.py
+++ count_function_alternative.py
@@ -1,7 +1,7 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
 # 1. Ask for user input.
-
+string_input = input("Enter any string: ")
 # 2. Assign which character to count.
 
 # 3. Create a loop to access every character in the input. 
