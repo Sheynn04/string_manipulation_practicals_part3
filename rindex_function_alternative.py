6ae212c560cee51e6a1065d56db8e6d1a3a8ff52
ycz <yczaubhreydy@gmail.com>
2025-04-01 22:04:40 +0800

    user inputs

--- rindex_function_alternative.py
+++ rindex_function_alternative.py
@@ -1,10 +1,10 @@
 # Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 
 #1. Ask for the user input.
-
+user_input = input("Input any string: ")
 
 # 2. Get the letter to find from the user.
-
+index = input("Letter to find: ")
 
 # 3. Count the characters before the indexed character.
 
