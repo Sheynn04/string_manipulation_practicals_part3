0e62c81a55f42c33c0384edcba782ac9df8d8878
ycz <yczaubhreydy@gmail.com>
2025-04-01 16:22:38 +0800

    ask user input and assigned suffix

--- removesuffix_alternative.py
+++ removesuffix_alternative.py
@@ -1,9 +1,9 @@
 # Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 
 # 1. Ask for the user inout.
-
+string_input = input("Input any string: ")
 # 2. Assign a suffix
-
+suffix = "dy"
 # 3. Remove the suffix from the user input without using removesuffix().
 
 # 4. Print the result.
