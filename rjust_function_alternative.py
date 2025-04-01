04e90caff3096e4fd109bd0285b1ab43fafcd4b2
ycz <yczaubhreydy@gmail.com>
2025-04-01 20:40:18 +0800

    pseudocode

---
+++ rjust_function_alternative.py
@@ -0,0 +1,9 @@
+# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+# 1. Ask for user input. 
+
+# 2. Assign a width for the statement.
+
+# 3. Add spaces to the user input if it's under the width
+
+# 4. Print the final output.
