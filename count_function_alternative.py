a37e6ed66b6ea0947cff40ceb04c454be2e76f2e
ycz <yczaubhreydy@gmail.com>
2025-04-01 21:10:57 +0800

    origin

---
+++ count_function_alternative.py
@@ -0,0 +1,11 @@
+# Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# 1. Ask for user input.
+
+# 2. Assign which character to count.
+
+# 3. Create a loop to access every character in the input. 
+
+# 4. Count the character you assigned from the user input.
+
+# 5. Print the count.
