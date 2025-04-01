6b05c21072e1f0c553d7298f2c9aa1c26998e673
ycz <yczaubhreydy@gmail.com>
2025-04-01 21:03:44 +0800

    zfill alternative (mathematical functions)

--- zfill_function_alternative.py
+++ zfill_function_alternative.py
@@ -1,10 +1,14 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
 # 1. Ask for user input.
-user_input = int(input("Enter numbers: "))
+user_input = input("Enter numbers: ")
 
 # 2. Assign a width to fill.
+width = 10
 
 # 3. Add zeros to the front if it's under the width.
+if len(user_input) < width:
+    user_input = "0" * (width - len(user_input)) + user_input
 
 # 4. Print the final output.
+print(user_input)
