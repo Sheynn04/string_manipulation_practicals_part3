1512c1712daad8902e907b27c5888607fa1b7bb4
ycz <yczaubhreydy@gmail.com>
2025-04-01 21:18:06 +0800

    count function alternative (for loop and if statement)

--- count_function_alternative.py
+++ count_function_alternative.py
@@ -2,10 +2,16 @@
 
 # 1. Ask for user input.
 string_input = input("Enter any string: ")
-# 2. Assign which character to count.
 
+# 2. Assign which character to count and the numbers of count.
+to_count = "e"
+count = 0
 # 3. Create a loop to access every character in the input. 
+for char in string_input:
 
 # 4. Count the character you assigned from the user input.
+    if char == to_count:
+        count += 1
 
 # 5. Print the count.
+print('"e" appeared: ', count)
