abd7065aac8160443fccb5ccf05dd93a13e9bfb6
ycz <yczaubhreydy@gmail.com>
2025-04-01 20:02:22 +0800

    added loop to access every characters

--- upper_function_alternative.py
+++ upper_function_alternative.py
@@ -1,10 +1,10 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
-# 1. Ask for user input
-
+# 1. Ask for user input.
+string_input = input("Input any string: ")
 
 # 2. Create a loop to access each character of the input.
-
+for char in string_input:
 
 # 3. Transform all lowercase characters into uppercase.
 
