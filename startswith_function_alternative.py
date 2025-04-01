389d9360afb7e9c010be952d73bb64dacf4cfb0b
ycz <yczaubhreydy@gmail.com>
2025-04-01 20:33:49 +0800

    startswith alternative (if-else statement)

--- startswith_function_alternative.py
+++ startswith_function_alternative.py
@@ -4,6 +4,11 @@
 string_input = input("Enter a string: ")
 
 # 2. Assign a variable for the prefix or the start with.
+starts_with = "dy"
 
 # 3. Check if the user input starts with the prefix. Print True if yes, print False if no.
+if string_input[:len(starts_with)] == starts_with:
+    print("True")
 
+else:
+    print("False")
