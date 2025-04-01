b0f43a7d529bee303ead7c359f8106312d7e501f
ycz <yczaubhreydy@gmail.com>
2025-04-01 16:01:06 +0800

    pseudocode

--- rstrip_alternative.py
+++ rstrip_alternative.py
@@ -1,1 +1,9 @@
-print('hello')
+# Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+# 1. Ask for the user input
+
+# 2. Create a loop so we can access every character in the input
+
+# 3. Remove the spaces at the end. 
+
+# 4. Print the result.
