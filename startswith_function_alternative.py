13b84dd91f7f27b244984fd8cc8194786d05a125
ycz <yczaubhreydy@gmail.com>
2025-04-01 20:29:11 +0800

    pseudocode

---
+++ startswith_function_alternative.py
@@ -0,0 +1,8 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# 1. Ask for user input.
+
+# 2. Assign a variable for the prefix or the start with.
+
+# 3. Check if the user input starts with the prefix. Print True if yes, print False if no.
+
