61e1c55c844a31d128f8d83b8b1e02412cabc748
ycz <yczaubhreydy@gmail.com>
2025-04-01 16:20:53 +0800

    pseudocode

---
+++ removesuffix_alternative.py
@@ -0,0 +1,9 @@
+# Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+
+# 1. Ask for the user inout.
+
+# 2. Assign a suffix
+
+# 3. Remove the suffix from the user input without using removesuffix().
+
+# 4. Print the result.
