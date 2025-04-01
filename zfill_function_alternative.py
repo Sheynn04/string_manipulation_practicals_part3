667d9d7989f5f529320b6a3b4625543977a7af5c
ycz <yczaubhreydy@gmail.com>
2025-04-01 20:58:55 +0800

    pseudocode and user input

---
+++ zfill_function_alternative.py
@@ -0,0 +1,10 @@
+# Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+
+# 1. Ask for user input.
+user_input = int(input("Enter numbers: "))
+
+# 2. Assign a width to fill.
+
+# 3. Add zeros to the front if it's under the width.
+
+# 4. Print the final output.
